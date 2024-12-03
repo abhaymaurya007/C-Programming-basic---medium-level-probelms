e876199f1ad5cc80b966ca4791ed8de43d983ea7
abhaymaurya007 <abhaymauryajnp1@gmail.com>
2024-12-03 22:00:06 +0530

    MINOR UPDATES
    BASIC C PROBLEMS

--- prob3.c
+++ prob3.c
@@ -1,17 +1,19 @@
-/*Write a C program to reverse a two digit number without using any loop. 
+/*Write a C program to reverse a two digit number without using any loop.
 The given number is 13 and the output is 31*/
-#include<stdio.h>
-void main(){
+#include <stdio.h>
+void main()
+{
     int num;
     printf("enter a two digit number :");
-    scanf("%d",&num);
-    if(num<0){
-        num=-num;
-    }
-    if(num>9 && num<100 )
+    scanf("%d", &num);
+    if (num < 0)
     {
-        printf("reverse of %d is %d%d ",num,num%10,num/10);
+        num = -num;
     }
-    else 
-    printf("enter 2 digit number only.");
+    if (num > 9 && num < 100)
+    {
+        printf("reverse of %d is %d%d ", num, num % 10, num / 10);
+    }
+    else
+        printf("enter 2 digit number only.");
 }
